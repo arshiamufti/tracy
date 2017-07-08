26fa8a0db6d33792dcbc0373f1a079c27e4bc960
arshiamufti <a2mufti@uwaterloo.ca>
2017-07-07 22:20:09 -0700

    improved hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+/*
+ * [chapter 2]
+ *
+ * Slightly improved hello world (used the Vec3 class)
+ */
+
+pub use self::vector::Vec3;
+mod vector;
+
 fn main() {
 
     let nx = 200; // rows
@@ -12,12 +21,14 @@ fn main() {
     // 0.0 to 1.0
     for j in (0..ny).rev() {
         for i in 0..nx {
-            let r = (i as f32)/(nx as f32);
-            let g = (j as f32)/(ny as f32);
-            let b = 0.2;
-            let ir = (255.99*r) as i32;
-            let ig = (255.99*g) as i32;
-            let ib = (255.99*b) as i32;
+            let v = Vec3 {
+                x: (i as f32)/(nx as f32),
+                y:  (j as f32)/(ny as f32),
+                z:  0.2
+            };
+            let ir = (255.99*v.x) as i32;
+            let ig = (255.99*v.y) as i32;
+            let ib = (255.99*v.z) as i32;
             println!("{} {} {}", ir, ig, ib);
         }
     }
