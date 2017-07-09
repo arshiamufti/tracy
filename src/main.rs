5a185df09f3261cb7652912497172cc66d3ff0b6
arshiamufti <a2mufti@uwaterloo.ca>
2017-07-08 23:10:02 -0700

    a simple sphere

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 /*
- * [chapter 3]
+ * [chapter 4]
  *
- * Rays, a simple camera, and a background
+ * Spheres!
  *
- * output: blue.png
+ * output: simplesphere.png
  */
 
 pub use self::vector::Vec3;
@@ -11,23 +11,34 @@ mod vector;
 pub use self::ray::Ray;
 mod ray;
 
+fn does_hit_sphere(center: Vec3, radius: f32, ray: &Ray) -> bool {
+    let direction = ray.direction();
+    let diff = ray.origin() - center;
+    let a = direction.dot(&direction);
+    let b = 2.0 * direction.dot(&diff);
+    let c = diff.dot(&diff) - (radius * radius);
+    let discriminant = (b * b) - (4.0 * a * c);
+
+    discriminant > 0.0
+}
 /*
  * This function accepts a ray and returns the colour of the background. The
  * colour is some gradient of blue depending on the y value of the ray's
- * direction. First, the direction vector is converted to a unit direction so
- * that -1.0 < y < 1.0. Then y is scaled to a t such that 0.0 < t < 1.0. When
- * t = 1.0, we want blue, when t = 0.0, we want white. When t is in between, we
- * want a "blend".
- *
- * (fun fact: the fancy word for a blend is a linear interpolation or 'lerp',
- * and it's of the form (1-t)*start_value + t*end_value), where t is in [0, 1]
+ * direction. (see more in simple_camera.rs)
  */
 fn color(ray: &Ray) -> Vec3 {
-    let unit_direction = ray.b.unit();
-    let t = 0.5*(unit_direction.y + 1.0);
-    let white = Vec3::from_one(1.0);
+    let red = Vec3 { x: 1.0, y: 0.0, z: 0.0 };
     let blue = Vec3 { x: 0.5, y: 0.7, z: 1.0 };
-    println!("unit_direction={}, t={}", unit_direction.y, t);
+    let white = Vec3::from_one(1.0);
+
+    let radius = 0.5;
+    let center = Vec3 { x: 0.0, y: 0.0, z: -1.0 };
+    if does_hit_sphere(center, radius, &ray) {
+        return red;
+    }
+
+    let unit_direction = ray.direction().unit();
+    let t = 0.5*(unit_direction.y + 1.0);
     (1.0-t)*white + t*blue
 }
 
